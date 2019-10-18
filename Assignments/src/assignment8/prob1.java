25308bdb38ac9b9ea8ea701c6cde72402133f699
thierry.manzi <thierry.manzi001@gmail.com>
2019-10-18 07:56:27 -0500

    ass8prob1

---
+++ Assignments/src/assignment8/prob1.java
@@ -0,0 +1,20 @@
+package assignment8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class prob1 {
+	public static void main(String[] args) {
+		BiFunction<Double,Double, List> bi = (x,y) -> {     
+			List<Double> list = new ArrayList<>();    
+			list.add(Math.pow(x,y));     
+			list.add(x * y);     
+			return list; 
+			}; 			
+		List<Double> test = bi.apply(2.0, 3.0);
+		System.out.println(test);
+	}
+
+}
+//Output: [8.0, 6.0]
