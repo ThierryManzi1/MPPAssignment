27dbf0e06f202dfde5ad6d0e608e8bc73cff8af1
thierry.manzi <thierry.manzi001@gmail.com>
2019-10-18 20:12:24 -0500

    sorted

--- Assignments/src/assignment8/Day1/prob2/taskD/ProductInfo.java
+++ Assignments/src/assignment8/Day1/prob2/taskD/ProductInfo.java
@@ -5,16 +5,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class ProductInfo {
-	static enum SortMethod {BYPRICE, BYTITLE};
-	SortMethod method;
 	
-	public static void  sort(List<Product> prod, SortMethod method) {
+	public static void  sort(List<Product> prod) {
 		
 		Collections.sort(prod, (p1,p2)->{
-			if( method == SortMethod.BYPRICE) {
-				return Double.compare(p1.price, p2.price);
-			}else {
+			if(!(p1.title.equals(p2.title))) {
 				return p1.title.compareTo(p2.title);
+			}else {
+				return Integer.compare(p1.model, p2.model);
 			}
 		});
 	
@@ -22,22 +20,12 @@ public class ProductInfo {
 	public static void main(String[] args) {
 		List<Product> prod = new ArrayList<>();
 		prod.add(new Product("Galaxy", 500.0,3));
-		prod.add(new Product("Mac", 1500.0,4));
+		prod.add(new Product("Galaxy", 1500.0,2));
 		prod.add(new Product("HP", 1800.0,6));
+		prod.add(new Product("Toshiba", 1800.0,6));
 		
-		
-		ProductInfo.sort(prod, SortMethod.BYPRICE);
-		
+		sort(prod);
 		System.out.println(prod);
-		
-		ProductInfo.sort(prod, SortMethod.BYTITLE);
-		System.out.println(prod);
-		
-		
-		
-	
-		
-	
 		
 	}
 
